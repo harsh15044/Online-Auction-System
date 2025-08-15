44cce24362a7a9e9c2563605bbb860afabbd3caa
harsh15044 <75984501+harsh15044@users.noreply.github.com>
2025-08-15 22:16:20 +0530

    ..

---
+++ src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/layouts/navbar"
+
+function Dashboard() {
+  return (
+    <>
+        <Navbar/>
+        <div className="hero-section">
+            
+        </div>
+    </>
+  )
+}
+
+export default Dashboard
